daefb3e53192171bd6bb5d54c3b189a52aab0667
pg1985 <patrick@pushup.com>
2015-07-06 14:59:45 -0500

    Can now scrape meta-data

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
 var express = require('express');
-var fs = require('fs');
 var request = require('request');
-var cheerio = require('cheerio');
+var url     = require('url');
+
+var Scraper = require('./lib/scraper.js');
+var scraper = new Scraper();
+
 var app = express();
 
-app.get('/scrape', function (req, res){
-	console.log(req['url'])
-	res.end("This works!");
-})
+app.get('/', function (req, res){
+
+	//get all URL data from the request, then parse out the query string argument.
+	url_data = url.parse(req.url, true);
+	url_query = String(url_data['query']['url']);
+
+	console.log(url_query);
+
+	//make the actual request.  This only needs to happen once
+	request(String(url_query), function(error, response, html){
+		if (!error) {
+
+			scraper.scrape_metadata(html);
+			scraper.scrape_html(html);
+
+			res.end("Ok")
+
+		} else {
+			console.log(error);
+		}
+	});
+});
 
 app.listen('9876');
 
-console.log('Bizzam this works.');
+console.log('Scraper is now listening on port 9876...\n');
 
 exports = module.exports = app;
